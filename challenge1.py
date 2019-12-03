d7475f5ae930663855581a8846e5d37cdec5e313
Jason Douglas Earl <jearl@notengoamigos.org>
2019-12-03 10:20:02 -0700

    Let's get started.

---
+++ challenge1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import unittest
+from selenium import webdriver
+
+class Challenge1(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("venv/chromedriver")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge1(self):
+        self.driver.get("https://www.google.com")
+        self.assertIn("Google", self.driver.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
