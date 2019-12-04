fd029e81ce80356a9d808de21c83171a81c0770b
Jason Douglas Earl <jearl@notengoamigos.org>
2019-12-04 11:13:52 -0700

    They didn't really ask for a test.

--- challenge3.py
+++ challenge3.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python3
 
 import time
-import unittest
 from selenium import webdriver
 
 
-class TestExotics(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome("venv/chromedriver")
-
-    def tearDown(self):
-        self.driver.close()
-
-    def test_popular_models(self):
-        self.driver.get("https://www.copart.com/")
-        time.sleep(1)
-        trending = self.driver.find_element_by_id("tabTrending")
-        links = trending.find_elements_by_tag_name("a")
-        for link in links:
-            href = link.get_attribute("href")
-            if "make" in href or "model" in href:
-                print("{} - {}".format(link.text, href))
+def main():
+    driver = webdriver.Chrome("venv/chromedriver")
+    driver.get("https://www.copart.com/")
+    time.sleep(1)
+    trending = driver.find_element_by_id("tabTrending")
+    links = trending.find_elements_by_tag_name("a")
+    for link in links:
+        href = link.get_attribute("href")
+        if "make" in href or "model" in href:
+            print("{} - {}".format(link.text, href))
+    driver.close()
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
