8e976fcfab0b78a47bd9404b2042a32510bf60f2
Jason Douglas Earl <jearl@notengoamigos.org>
2019-12-04 10:14:20 -0700

    Challenge 2 finished.

---
+++ challenge2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class TestExotics(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome("venv/chromedriver")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_exotics(self):
+        self.driver.get("https://www.copart.com/")
+        self.driver.find_element(By.ID, "input-search").click()
+        self.driver.find_element(By.ID, "input-search").send_keys("exotics")
+        self.driver.find_element(By.ID, "input-search").send_keys(Keys.ENTER)
+        time.sleep(5)
+        table = self.driver.find_element_by_id("serverSideDataTable")
+        self.assertIn("PORSCHE", table.text)
+
+if __name__ == '__main__':
+    unittest.main()
